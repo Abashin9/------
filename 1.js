dc3c346e627ac7ea2a8fc0ac1d0834f7d95cea5c
Andrei Abashin <aabashin@sfedu.ru>
2024-09-09 20:29:11 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,38 @@
+document.getElementById("bmw5").addEventListener('mouseover', function(){
+    this.classList.toggle('zoomed');
+});
+document.getElementById("bmw5").addEventListener('mouseout', function(){
+    this.classList.toggle('unzoomed');
+});
+
+
+
+document.getElementById("bmw4").addEventListener('mouseover', function(){
+    this.classList.toggle('zoomed');
+});
+document.getElementById("bmw4").addEventListener('mouseout', function(){
+    this.classList.toggle('unzoomed');
+});
+
+
+
+document.getElementById("bmwx5").addEventListener('mouseover', function(){
+    this.classList.toggle('zoomed');
+});
+document.getElementById("bmwx5").addEventListener('mouseout', function(){
+    this.classList.toggle('unzoomed');
+});
+
+document.getElementById("bmw7").addEventListener('mouseover', function(){
+    this.classList.toggle('zoomed');
+});
+document.getElementById("bmw7").addEventListener('mouseout', function(){
+    this.classList.toggle('unzoomed');
+});
+
+document.getElementById("bmwx7").addEventListener('mouseover', function(){
+    this.classList.toggle('zoomed');
+});
+document.getElementById("bmwx7").addEventListener('mouseout', function(){
+    this.classList.toggle('unzoomed');
+});
